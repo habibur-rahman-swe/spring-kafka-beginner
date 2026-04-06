4d60af07ecd7a4899110af399a69e0c0b39f3b9c
Sharif Uddin <suddin@connexpay.com>
2026-04-06 14:26:06 +0600

    producer config for object- extra look at pom.xml file

--- producer/src/main/java/com/demo/producer/controller/KafkaProducerController.java
+++ producer/src/main/java/com/demo/producer/controller/KafkaProducerController.java
@@ -2,25 +2,28 @@ package com.demo.producer.controller;
 
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.producer.model.RiderLocation;
 
 @RestController
 @RequestMapping("/api/producer")
 public class KafkaProducerController {
 
-	private final KafkaTemplate<String, String> kafkaTemplate;
+	private final KafkaTemplate<String, RiderLocation> kafkaTemplate;
 
-	public KafkaProducerController(KafkaTemplate<String, String> kakfaTemplate) {
+	public KafkaProducerController(KafkaTemplate<String, RiderLocation> kakfaTemplate) {
 		this.kafkaTemplate = kakfaTemplate;
 	}
 
 	@PostMapping("/send")
-	public String sendMessage(@RequestParam String message) {
-		kafkaTemplate.send("my-topic", message);
+	public String sendMessage(@RequestBody RiderLocation riderLocation) {
 		
-		return "Message sent to Kafka: " + message;
+		kafkaTemplate.send("rider-topic", riderLocation);
+		
+		return "Message sent to Kafka: " + riderLocation.toString();
 	}
 
 }
