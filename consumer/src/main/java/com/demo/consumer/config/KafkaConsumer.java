479130d59a470a9a4c76f2f4355ab728595509c2
Sharif Uddin <suddin@connexpay.com>
2026-04-06 15:13:42 +0600

    consumer receive message objects

--- consumer/src/main/java/com/demo/consumer/config/KafkaConsumer.java
+++ consumer/src/main/java/com/demo/consumer/config/KafkaConsumer.java
@@ -3,17 +3,24 @@ package com.demo.consumer.config;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.demo.consumer.model.RiderLocation;
+
 @Component
 public class KafkaConsumer {
 	
-	@KafkaListener(topics = "my-topic", groupId = "my-group")
-	public void listen1(String message) {
-		System.out.println("Consumer1: " + message);
+	@KafkaListener(topics = "rider-topic", groupId = "my-group")
+	public void listen1(RiderLocation message) {
+		System.out.println("Consumer1: " + message.toString());
 	}
 	
-	@KafkaListener(topics = "my-topic", groupId = "my-group")
-	public void listen2(String message) {
-		System.out.println("Consumer2: " + message);
+	@KafkaListener(topics = "rider-topic", groupId = "my-group")
+	public void listen2(RiderLocation message) {
+		System.out.println("Consumer2: " + message.toString());
+	}
+	
+	@KafkaListener(topics = "rider-topic", groupId = "my-group")
+	public void listen3(RiderLocation message) {
+		System.out.println("Consumer3: " + message.toString());
 	}
 
 }
---
+++ consumer/src/main/java/com/demo/consumer/model/RiderLocation.java
@@ -0,0 +1,48 @@
+package com.demo.consumer.model;
+
+public class RiderLocation {
+	private String riderId;
+	private double latitude;
+	private double longitude;
+
+	public String getRiderId() {
+		return riderId;
+	}
+
+	public void setRiderId(String riderId) {
+		this.riderId = riderId;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public RiderLocation(String riderId, double latitude, double longitude) {
+		super();
+		this.riderId = riderId;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public RiderLocation() {
+
+	}
+
+	@Override
+	public String toString() {
+		return "RiderLocation [riderId=" + riderId + ", latitude=" + latitude + ", longitude=" + longitude + "]";
+	}
+
+}
