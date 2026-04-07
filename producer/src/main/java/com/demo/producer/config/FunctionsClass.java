c34aac046f50a0ea3fe44a0db96a7ec1be478525
Sharif Uddin <suddin@connexpay.com>
2026-04-07 11:49:31 +0600

    functions(spring cloud)

---
+++ producer/src/main/java/com/demo/producer/config/FunctionsClass.java
@@ -0,0 +1,18 @@
+package com.demo.producer.config;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FunctionsClass {
+
+	@Bean
+	public Function<String, String> uppercase() {
+		return value -> value.toUpperCase();
+	}
+	
+	
+	
+}
